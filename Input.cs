46d777d1b99fe592f6de37656bca40273bd8ecc6
Danne69420 <Daniel.kindvall05@gmail.com>
2022-04-26 11:02:45 +0200

    Add Input class
    Not tested. Not quite sure how or if it works yet.

---
+++ Input.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections;
+using System.Windows.Forms;
+namespace Snake
+{
+    class Input             //This class reads the users keyboard input. Somehow.
+    {
+        private static Hashtable keyTable = new Hashtable();        //This is supposed to be the most efficient way to do this. Problem is i dont really understand how it works.
+        public static bool KeyPress (Keys key)
+        {
+            if (keyTable[key] == null)
+            {
+                return false;
+            }
+            return (bool)keyTable[key];
+        }
+        public static void ChangeState(Keys key, bool state)
+        {
+            keyTable[key] = state;
+        }
+    }
+}
